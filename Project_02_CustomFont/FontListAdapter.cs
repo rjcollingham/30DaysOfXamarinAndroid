f8d05857e016d18feeb5410e6095e3959612cf05
rjcollingham <russell@MacBook-Pro.local>
2016-06-25 16:03:47 +0100

    added project 02 custom font

---
+++ Project_02_CustomFont/FontListAdapter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Android.App;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Views;
+using Android.Widget;
+
+namespace Project_02_CustomFont
+{
+	public class FontListAdapter : BaseAdapter<string>
+	{
+		string[] fonts;
+		string[] items;
+		int FontIndex = 0;
+		Activity context;
+
+		public FontListAdapter(Activity context, string[] items, string[] fonts) : base()
+		{
+			this.context = context;
+			this.items = items;
+			this.fonts = fonts;
+		}
+
+		public int ChangeFont()
+		{
+			FontIndex = (FontIndex + 1) % fonts.Length;
+			return FontIndex;
+		}
+
+		public override string this[int position]
+		{
+			get
+			{
+				return items[position];
+			}
+		}
+
+		public override int Count
+		{
+			get
+			{
+				return items.Length;
+			}
+		}
+
+		public override long GetItemId(int position)
+		{
+			return position;
+		}
+
+		public override View GetView(int position, View convertView, ViewGroup parent)
+		{
+			View view = convertView;
+			if (view == null)
+			{
+				view = context.LayoutInflater.Inflate(Android.Resource.Layout.SimpleListItem1, null);
+			}
+
+			var cell = view.FindViewById<TextView>(Android.Resource.Id.Text1);
+			cell.Text = items[position];
+			cell.SetBackgroundColor(Color.Black);
+			cell.SetTextColor(Color.White);
+			cell.SetTypeface(Typeface.CreateFromAsset(context.Assets, fonts[FontIndex] + ".ttf"), TypefaceStyle.Normal);
+
+			return view;
+		}
+	}
+}
+
---
+++ Project_02_CustomFont/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using System;
+
+namespace Project_02_CustomFont
+{
+	[Activity(Label = "", Theme = "@android:style/Theme.Material",
+	          MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Activity
+	{
+		List<string> _sentences = new List<string>
+		{
+			"I wandered lonely as a cloud",
+			"That floats on high o'er vales and hills,",
+			"When all at once I saw a crowd,",
+			"A host, of golden daffodils;",
+			"Beside the lake, beneath the trees,",
+			"Fluttering and dancing in the breeze."
+		};
+
+		List<string> _fonts = new List<string>
+		{
+			"PT-Sans-Web-Regular",
+			"Raleway-Regular",
+			"Ubuntu-Regular",
+			"BebasNeueRegular",
+			"Cabin-Regular",
+			"Lato-Regular",
+			"Montserrat-Regular",
+			"OpenSans-Regular",
+		};
+
+		ListView lvSentences;
+		FontListAdapter flaSentences;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView(Resource.Layout.Main);
+
+			Button btnChange = FindViewById<Button>(Resource.Id.btnChange);
+		 	lvSentences = FindViewById<ListView>(Resource.Id.lvSentences);
+
+			flaSentences = new FontListAdapter(this, _sentences.ToArray(), _fonts.ToArray());
+			lvSentences.Adapter = flaSentences;
+
+			btnChange.Click += Change_Font;
+			this.Title = _fonts[0];
+		}
+
+		void Change_Font(object sender, EventArgs e)
+		{
+			int ix = flaSentences.ChangeFont();
+			this.Title = _fonts[ix];
+			flaSentences.NotifyDataSetChanged();
+		}
+	}
+}
+
+
